7111231c7a8037078a6aa810f1da94624ee805b7
hoanglong45 <cuncongame@gmail.com>
2022-04-28 11:02:38 +0700

    UI and function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Web3 from "web3/dist/web3.min.js";
+import { useState, useEffect } from "react";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 function App() {
+  const [web3API, setWeb3API] = useState({
+    provider: null,
+    web3: null,
+  });
+
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+
+      if (provider) {
+        setWeb3API({
+          web3: new Web3(provider),
+          provider,
+        });
+      } else {
+        console.error("Please install Metamask");
+      }
+    };
+    loadProvider();
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3API.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    };
+    web3API.web3 && getAccount();
+  }, [web3API.web3]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>10 ETH</strong>
+        </div>
+        <button className="button is-primary mr-5">Donate</button>
+        <button className="button is-danger mr-5">Withdraw</button>
+        <button
+          className="button is-link"
+          onClick={() =>
+            web3API.provider.request({ method: "eth_requestAccounts" })
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Connect Wallets
+        </button>
+        <span>
+          <p>
+            <strong>Account Address: </strong>
+            {account ? account : "Account denied"}
+          </p>
+        </span>
+      </div>
     </div>
   );
 }
